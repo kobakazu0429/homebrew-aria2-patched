2161cb63a717791d979280da851ca0b35530c0a8
kobakazu0429 <kobakazu10@gmail.com>
2023-12-03 13:35:23 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homebrew-aria2-patched
+
+patched aria2 for homebrew
+
+## Patches
+
+- [max-connection-to-unlimited.patch](./max-connection-to-unlimited.patch)
+
+## Licence
+
+- aria2-patched.rb is licensed mainly under the [BSD-2-Clause license](https://github.com/Homebrew/homebrew-core/blob/master/LICENSE.txt).
+- Others are licensed under the [MIT license](./LICENSE).
---
+++ generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# -e: コマンドが失敗した時点でスクリプト全体を即座にエラー終了する
+# -u: 初期化していない変数があるとエラーにしてくれる
+# -x: 実行するコマンドを出力してくれる
+set -eux
+
+LICENSE_TEXT="# Licensed under the BSD-2-Clause license. (https://github.com/Homebrew/homebrew-core/blob/master/LICENSE.txt)"
+BASE_FORMULA="https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/a/aria2.rb"
+FORMULA_FILENAME="aria2-patched.rb"
+PATCH_FILENAME="max-connection-to-unlimited.patch"
+
+if [ -e $FORMULA_FILENAME ]; then
+  rm $FORMULA_FILENAME
+fi
+wget $BASE_FORMULA -O $FORMULA_FILENAME
+
+cat <<EOF > $FORMULA_FILENAME
+# Licensed under the BSD-2-Clause license.
+# https://github.com/Homebrew/homebrew-core/blob/master/LICENSE.txt
+
+$(sed -e '$d' $FORMULA_FILENAME)
+
+  # The lines below this have been modified by kobakazu0429.
+  patch do
+    url "https://raw.githubusercontent.com/kobakazu0429/homebrew-aria2-patched/master/$PATCH_FILENAME",
+    sha256 "$(sha256sum $PATCH_FILENAME | cut -f 1  -d ' ')"
+  end
+end
+EOF
